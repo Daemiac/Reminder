17cf07bc71d0b1261648e7bd23ee0631c08d9b2f
Moony <51023622+ApprenticeRoboticist@users.noreply.github.com>
2021-02-17 14:58:45 +0100

    Label, List Widget and 2 buttons at the bottom. First tryout of layout setting

--- Reminder/reminder.py
+++ Reminder/reminder.py
@@ -1,11 +1,11 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 import sys
 
 
-class AppWindow(QMainWindow):
+class AppWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.title = "Reminder alpha ver"
@@ -13,33 +13,54 @@ class AppWindow(QMainWindow):
         self.y_position = 400
         self.width = 600
         self.height = 400
+        self.task_list = ["My first task", "My second task", "My third task"]
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.x_position, self.y_position, self.width, self.height)
-        #self.setWindowIcon(QIcon('python.png'))
-
-        """ initialize buttons"""
-        self.btn1 = QtWidgets.QPushButton("Greet user", self)
-        self.btn1.setGeometry(250, 50, 100, 50)
-        self.btn1.clicked.connect(self.btn1_clicked)
-        self.btn2 = QtWidgets.QPushButton("Exit app", self)
-        self.btn2.setStyleSheet('color:red')
-        self.btn2.setGeometry(250, 300, 100, 50)
-        self.btn2.clicked.connect(self.btn2_clicked)
 
         """initialize labels"""
         self.lbl1 = QtWidgets.QLabel(self)
         self.lbl1.setAlignment(Qt.AlignCenter)
-        self.lbl1.setText("Click the button above to see the message!")
+        self.lbl1.setText("List of your current tasks")
         self.lbl1.adjustSize()
-        self.lbl1.move(180, 120)
+        #self.lbl1.move(180, 120)
 
         """initialize text field"""
-        self.text1 = QtWidgets.QLineEdit(self)
-        self.text1.setGeometry(250, 150, 100, 50)
-        self.text1.setAlignment(Qt.AlignCenter)
+        #self.text1 = QtWidgets.QLineEdit(self)
+        #self.text1.setGeometry(250, 150, 100, 50)
+        #self.text1.setAlignment(Qt.AlignCenter)
+
+        """ initialize list widget"""
+        self.quest_list = QtWidgets.QListWidget(self)
+        for task in self.task_list:
+            self.quest_list.addItem(task)
+        self.quest_list.adjustSize()
+
+        """ initialize buttons"""
+        self.btn1 = QtWidgets.QPushButton("Greet user", self)
+        # self.btn1.setGeometry(250, 50, 100, 50)
+        self.btn1.clicked.connect(self.btn1_clicked)
+
+        self.btn2 = QtWidgets.QPushButton("Exit app", self)
+        self.btn2.setStyleSheet('color:red')
+        # self.btn2.setGeometry(250, 300, 100, 50)
+        self.btn2.clicked.connect(self.btn2_clicked)
+
+        """ layout objects """
+        vbox = QVBoxLayout()
+        hbox = QHBoxLayout()
+        vbox.addWidget(self.lbl1)
+        vbox.addWidget(self.quest_list)
+        vbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn2)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.x_position, self.y_position, self.width, self.height)
+        # self.setWindowIcon(QIcon('python.png'))
 
     def btn1_clicked(self):
         self.text1.setText("Hello user!")
