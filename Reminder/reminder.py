a8bce834ae458d1dba07a42e8705bcbbc79159b3
Moony <51023622+ApprenticeRoboticist@users.noreply.github.com>
2021-03-08 19:48:57 +0100

    Code refactor: - commented all existing methods; - Moved "test_tasks" to external file in tests folder;

--- Reminder/reminder.py
+++ Reminder/reminder.py
@@ -20,11 +20,6 @@ STYLE_SHEET = {'app_window': 'background-color: #121212;',
                'mot_quote': 'background-color: #800000; color: white; font-weight: bold; font-family:'
                             ' Courier; font-size: 8pt; border: 1px solid black;', }
 
-TASKS = {'task list': [{'Back to home': 'Going back to home for a weekend'},
-                       {'Buy potatoes': 'You are short on potatoes. You can\'t make french fries without them!'},
-                       {'Clean your room': 'Your environment is messy. Dustin\' time'},
-                       {'Try if any errors occur': 'Testing json method'}], }
-
 
 class AppView(QMainWindow):
     """ Class responsible for rendering view of the app """
@@ -37,6 +32,7 @@ class AppView(QMainWindow):
         self.width = 1100
         self.height = 600
         self.setWindowTitle(self.title)
+        # TODO set proper window size and adjust the layout of the window widget
         self.setGeometry(self.x_position, self.y_position, self.width, self.height)
         self.setStyleSheet(STYLE_SHEET['app_window'])
 
@@ -81,6 +77,7 @@ class TaskListWidget(QWidget):
         self.setLayout(self.main_layout)
 
     def create_task_list_label(self):
+        """ Sets up task list label and its properties """
         self.task_list_label = QtWidgets.QLabel(self)
         self.task_list_label.setAlignment(Qt.AlignCenter)
         self.task_list_label.setText("List of your current tasks")
@@ -89,12 +86,14 @@ class TaskListWidget(QWidget):
         self.main_layout.addWidget(self.task_list_label)
 
     def create_task_list_widget(self):
+        """ Sets up task list widget and its properties """
         self.task_list_widget = QtWidgets.QListWidget(self)
         self.task_list_widget.setMinimumSize(250, 400)
         self.task_list_widget.setStyleSheet(STYLE_SHEET['task_list_widget'])
         self.main_layout.addWidget(self.task_list_widget)
 
     def create_buttons(self):
+        """ Sets up widget's buttons and their properties """
         button_layout = QHBoxLayout()
         self.add_button = QtWidgets.QPushButton("Add task", self)
         self.add_button.setStyleSheet(STYLE_SHEET['button'])
@@ -142,6 +141,7 @@ class TaskInfoWidget(QWidget):
         self.setLayout(self.main_layout)
 
     def create_task_details_label(self):
+        """ Sets up task detail widget's label and its properties """
         self.task_details_label = QtWidgets.QLabel(self)
         self.task_details_label.setAlignment(Qt.AlignCenter)
         self.task_details_label.setText("Details of your task")
@@ -151,6 +151,7 @@ class TaskInfoWidget(QWidget):
         self.main_layout.addWidget(self.task_details_label)
 
     def create_task_details_widget(self):
+        """ Sets up task detail widget and its properties """
         self.quest_text = QtWidgets.QTextBrowser(self)
         self.quest_text.append("Select a task to show its details here!")
         self.quest_text.setStyleSheet(STYLE_SHEET['task_details_widget'])
@@ -158,6 +159,7 @@ class TaskInfoWidget(QWidget):
         self.main_layout.addWidget(self.quest_text)
 
     def update_quest_info(self, item_clicked):
+        """ Method responsible for updating task detail widget's content """
         self.quest_text.setText("Details: {}".format(item_clicked))
 
 
@@ -175,6 +177,7 @@ class BottomWidget(QWidget):
         self.setLayout(self.main_layout)
 
     def create_motivational_quote(self):
+        """ Sets up motivational quote widget and its properties """
         self.mot_quote = QtWidgets.QLabel(self)
         self.mot_quote.setAlignment(Qt.AlignCenter)
         self.mot_quote.setStyleSheet(STYLE_SHEET['mot_quote'])
@@ -183,6 +186,7 @@ class BottomWidget(QWidget):
         self.main_layout.addWidget(self.mot_quote)
 
     def create_close_button(self):
+        """ Sets up additional app's close button and its properties """
         self.close_button = QtWidgets.QPushButton("Close app", self)
         self.close_button.setStyleSheet(STYLE_SHEET['button'])
         self.close_button.setFixedWidth(100)
@@ -191,11 +195,12 @@ class BottomWidget(QWidget):
         self.main_layout.addWidget(self.close_button)
 
     def set_motivational_quote(self, quote_dic):
+        """ Method responsible for updating motivational quote widget's content """
         self.mot_quote.setText(quote_dic["quote"] + " - " + quote_dic["author"])
 
 
 class AddDialog(QDialog):
-
+    """ Class used to create dialog window widgets """
     def __init__(self):
         super(AddDialog, self).__init__()
         self.setFixedWidth(500)
@@ -224,6 +229,7 @@ class AddDialog(QDialog):
         self.main_layout.addWidget(self.form_group_box)
 
     def create_button_box(self):
+        """ Sets up dialog's window bottom buttons and their properties """
         self.button_box = QDialogButtonBox(QDialogButtonBox.Save | QDialogButtonBox.Cancel)
         self.main_layout.addWidget(self.button_box)
 
@@ -238,33 +244,40 @@ class AppController:
         self._connect_signals()
 
     def _connect_signals(self):
+        """ Method responsible for connecting main windows' signals with appropriate slot methods """
         self._view.first_widget.add_button.clicked.connect(self.add_task_window)
         self._view.first_widget.arch_button.clicked.connect(self.delete_task)
         self._view.third_widget.close_button.clicked.connect(self._close_the_app)
         self._view.first_widget.task_list_widget.itemClicked.connect(self.update_task_info)
 
     def _connect_dialog_signals(self):
+        """ Method responsible for connecting dialog window's signals with appropriate slot methods """
         self._dialog.button_box.accepted.connect(self.accept_dialog)
         self._dialog.button_box.rejected.connect(self.reject_dialog)
 
     def _close_the_app(self):
+        """ Saves changes into external file and closes the app """
         self._model.save_tasks()
         print("Closing the app...")
         sys.exit()
 
     def update_task_list(self):
+        """ Updates task list widget's content with content of model's task list attribute """
         self._view.first_widget.show_tasks(self._model.task_list)
 
     def update_motivational_quote(self):
+        """ Updates motivational quote widget's content with appropriate models' attribute """
         chosen_quote = self._model.get_motivational_quote()
         self._view.third_widget.set_motivational_quote(chosen_quote)
 
     @staticmethod
     def obtain_task_list_item(item_clicked):
+        """ Returns clicked list widget's element as a string """
         item_clicked = str(item_clicked.text())
         return item_clicked
 
     def update_task_info(self, item_clicked):
+        """ Updates task info widget's content """
         self.item_clicked = self.obtain_task_list_item(item_clicked)
         # searching through list of dictionaries
         task_det_dic = next(d for i, d in enumerate(self._model.task_list) if self.item_clicked in d)
@@ -272,12 +285,13 @@ class AppController:
         self._view.second_widget.update_quest_info(task_info)
 
     def add_task_window(self):
+        """ Creates a dialog window which can be used to add a task """
         self._dialog = AddDialog()
         self._connect_dialog_signals()
         self._dialog.show()
 
     def accept_dialog(self):
-        # TODO implement taking values from dialog textedit fields
+        """ Saves dialog window's entries to model's attribute and closes dialog window """
         key = self._dialog.task_title_edit.text()
         value = self._dialog.task_details_edit.toPlainText()
         self._model.add_task_to_list(key, value)
@@ -286,10 +300,13 @@ class AppController:
         print("Dialog form has been accepted. The task has been added. Closing the dialog window...")
 
     def reject_dialog(self):
+        """ Closes dialog window without applying changes """
         self._dialog.close()
         print("Dialog form has been rejected. Closing the dialog window...")
 
     def delete_task(self):
+        """ Method responsible for deleting a task from model's task list attribute and updating
+            task list widget's view """
         item_to_delete = self.item_clicked
         self._model.delete_task_from_list(item_to_delete)
         print("task deleted successfully !")
@@ -303,6 +320,7 @@ class AppModel:
 
     @staticmethod
     def retrieve_tasks():
+        """ Retrieve tasks from external file """
         # TODO Handle an exception when there is not a single item in dictionary
         with open('task_list.txt', 'r') as read_file:
             data = json.load(read_file)
@@ -310,6 +328,7 @@ class AppModel:
             return data['task list']
 
     def save_tasks(self):
+        """ Saves made changes to external file """
         data = {"task list": self.task_list}
         with open('task_list.txt', 'w') as outfile:
             json.dump(data, outfile, indent=2)
@@ -317,6 +336,7 @@ class AppModel:
 
     @staticmethod
     def get_motivational_quote():
+        # TODO Make a model for quotes handling and implement getting a quote from api
         """ Gets random motivational quote from external file """
         with open('files/mot_quotes.txt', 'r') as read_file:
             data = json.load(read_file)
@@ -328,11 +348,17 @@ class AppModel:
             return mot_quote
 
     def add_task_to_list(self, title, details):
+        """ Modifies self.task_list attribute by adding given item """
         task = {title: details}
         self.task_list.append(task)
         print("A task has been added to list!")
 
+    def update_task(self):
+        """ Modifies self.task_list attribute by changing details of given item """
+        pass
+
     def delete_task_from_list(self, task):
+        """ Modifies self.task_list attribute by deleting given item """
         print(task)
         task_det = next(i for i, d in enumerate(self.task_list) if task in d)
         del self.task_list[task_det]
